77b3d058c0c4e7d483ad45556d7f225f149d9f32
Jamesb13579 <jamesbagge@ymail.com>
2022-01-23 00:44:25 +0000

    intial commit plus word liabary and some html

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+var wordOptions = [
+    "SPACEMAN",
+    "ABDUCTION",
+    "PROBE",
+    "SPACESHIP",
+    "ENCOUNTER",
+    "CONSPIRACY",
+    "GALAXY",
+    "INVASION",
+    "MARTIAN",
+    "PLANETS",
+    "SIGHTINGS",
+    "VOYAGE",
+    "CROPCIRCLE",
+    "MOTHERSHIP",
+    "ROSWELL",
+    "SPECIES",
+    "ASTEROID",
+    "ORBITING",
+    "EUROPA",
+    "UNIVERSE",
+    "TELESCOPE",
+    "PHENOMENON",
+    "SOLAR",
+    "LANDER",
+    "QUASARS",
+    "VISITED",
+    "WEAPONS",
+    "UNEXPLAINED",
+];
+
+/**
+ * pick random word from wordOptions
+ */
+
+let word = wordOptions[Math.floor(Math.random()*wordOptions.length)];
