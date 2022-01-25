bacc7ab8005548d9534357a793e5df0ff852807f
Jamesb13579 <jamesbagge@ymail.com>
2022-01-25 23:05:55 +0000

    edited javascript to twst functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,9 +29,61 @@ var wordOptions = [
     "UNEXPLAINED",
 ];
 
+
+let score = document.getElementById("guesses-left")
 /**
  * pick random word from wordOptions
  */
+ const game = 
+    function chooseWord() {
+        word = wordOptions[Math.floor(Math.random()*wordOptions.length)];;
+            game.start();
+        },
 
-let word = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+function start() {
+    word = word.toUpperCase();
+        $("#main-image").attr("src", "assets/images/alien-8.jpg");
+    $("#randomWord").html(output);
+    }
 
+function LetterChoice (letter) {
+    let error = true;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == letter) {
+            output[i] = letter;
+            error = false;
+            game.rightGuess();
+        }
+    }
+    if (error) {
+        game.wrongGuess();
+    }
+}
+
+// update displayed word and score
+function rightGuess () {
+    $("#random-word").html(output);
+        // Check winning condition
+    if (output.toString().replace(/,/g, "") == word) {
+        $("#outcome").text("Well Done!");
+        game.over();
+    }
+}
+
+//  update image and score
+function wrongGuess () {
+    $("#main-image").attr("src", `assets/images/alien-` + score + `.jpg`);
+    score -= 1;
+    if (points < 0) { points = 0; }
+    // Check losing condition
+    if (score == 0) {
+        $("#outcome").text("You Lost...");
+        game.over();
+        $("#main-image").attr("src", `assets/images/alien-` + score + `.jpg`);
+    }
+}
+
+$(".btn-key").click(function () {
+    $(this).attr("disabled", true);
+    game.chooseLetter(this.textContent);
+});
