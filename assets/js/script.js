83237618a19de4b1f395305f1159542fb055f465
Jamesb13579 <jamesbagge@ymail.com>
2022-01-27 03:11:55 +0000

    add funtion for keyboard and tested

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,27 +29,59 @@ var wordOptions = [
     "UNEXPLAINED",
 ];
 
-let answer = '';
-let maxWrongGuesses = 8;
+let wordChoice = '';
+let maxWrong = 6;
 let mistakes = 0;
-let guessedLetters = [];
-
+let guessed = [];
+let underlined = null;
 
 /**
  * pick random word from wordOptions
  */
 function chooseWord() {
-    word = wordOptions[Math.floor(Math.random()*wordOptions.length)];
-    alert(word)
+
+    wordChoice = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    alert(wordChoice)
     }
 
-function guessedWord() {
-    amountOfLetters = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    function guessedWord() {
+        underlined = wordChoice.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
       
-    document.getElementById('random-word').innerHTML = amountOfLetters;
-    }
-
+        document.getElementById('random-word').innerHTML = underlined;
+      }
 document.getElementById('maxWrongGuesses').innerHTML = maxWrongGuesses;
 
+function generateButtons() {
+    let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+  }
+  
+  function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+  
+    if (wordChoice.indexOf(chosenLetter) >= 0) {
+      guessedWord();
+      checkIfGameWon();
+    } else if (wordChoice.indexOf(chosenLetter) === -1) {
+      mistakes++;
+      updateMistakes();
+      checkIfGameLost();
+      updateHangmanPicture();
+    }
+  }
+
 chooseWord();
+generateButtons() 
 guessedWord();
