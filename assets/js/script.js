2318b2cd38a23f937b61f201f122e93e3dc05d96
Jamesb13579 <jamesbagge@ymail.com>
2022-01-27 01:31:24 +0000

    add function for guessing letter

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,61 +29,27 @@ var wordOptions = [
     "UNEXPLAINED",
 ];
 
+let answer = '';
+let maxWrongGuesses = 8;
+let mistakes = 0;
+let guessedLetters = [];
 
-let score = document.getElementById("guesses-left")
+
 /**
  * pick random word from wordOptions
  */
- const game = 
-    function chooseWord() {
-        word = wordOptions[Math.floor(Math.random()*wordOptions.length)];;
-            game.start();
-        },
-
-function start() {
-    word = word.toUpperCase();
-        $("#main-image").attr("src", "assets/images/alien-8.jpg");
-    $("#randomWord").html(output);
+function chooseWord() {
+    word = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+    alert(word)
     }
 
-function LetterChoice (letter) {
-    let error = true;
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] == letter) {
-            output[i] = letter;
-            error = false;
-            game.rightGuess();
-        }
+function guessedWord() {
+    amountOfLetters = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+      
+    document.getElementById('random-word').innerHTML = amountOfLetters;
     }
-    if (error) {
-        game.wrongGuess();
-    }
-}
 
-// update displayed word and score
-function rightGuess () {
-    $("#random-word").html(output);
-        // Check winning condition
-    if (output.toString().replace(/,/g, "") == word) {
-        $("#outcome").text("Well Done!");
-        game.over();
-    }
-}
+document.getElementById('maxWrongGuesses').innerHTML = maxWrongGuesses;
 
-//  update image and score
-function wrongGuess () {
-    $("#main-image").attr("src", `assets/images/alien-` + score + `.jpg`);
-    score -= 1;
-    if (points < 0) { points = 0; }
-    // Check losing condition
-    if (score == 0) {
-        $("#outcome").text("You Lost...");
-        game.over();
-        $("#main-image").attr("src", `assets/images/alien-` + score + `.jpg`);
-    }
-}
-
-$(".btn-key").click(function () {
-    $(this).attr("disabled", true);
-    game.chooseLetter(this.textContent);
-});
+chooseWord();
+guessedWord();
